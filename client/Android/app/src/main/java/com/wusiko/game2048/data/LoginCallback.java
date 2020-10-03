cdba53413b55f760b849b228c17c23836676b08a
Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2020-10-03 16:39:20 +0700

    remove extra java class

--- client/Android/app/src/main/java/com/wusiko/game2048/data/LoginCallback.java
+++
@@ -1,7 +0,0 @@
-package com.wusiko.game2048.data;
-
-import com.wusiko.game2048.data.model.LoggedInUser;
-
-public abstract class LoginCallback {
-    public abstract void run(Result<LoggedInUser> result);
-}
