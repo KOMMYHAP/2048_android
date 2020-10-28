bc4f01216ec7d7e3cec43638d12be8b28f6d2aa0
KOMMYHAP <pasha.u.polyak@gmail.com>
2020-10-29 01:30:08 +0700

    apply code formatter changes

--- client/Android/app/src/main/java/com/wusiko/game2048/data/game/GameTileFactory.java
+++ client/Android/app/src/main/java/com/wusiko/game2048/data/game/GameTileFactory.java
@@ -2,40 +2,48 @@ package com.wusiko.game2048.data.game;
 
 import java.util.Random;
 
-public class GameTileFactory {
-    private float[] mProbabilities;
-    private Random mRandom;
+public class GameTileFactory
+{
+	private final float[] mProbabilities;
+	private final Random mRandom;
 
-    public GameTileFactory(final float[] probabilities, Random random) {
-        if (probabilities == null || probabilities.length > GameConfig.FIELD_SIZE) {
-            throw new RuntimeException("Invalid probabilities of tile creation: too less or too much tiles!");
-        }
+	public GameTileFactory(final float[] probabilities, Random random)
+	{
+		if (probabilities == null || probabilities.length > GameConfig.FIELD_SIZE)
+		{
+			throw new RuntimeException("Invalid probabilities of tile creation: too less or too much tiles!");
+		}
 
-        float sumProb = 0.0f;
-        for (final float prob : probabilities) {
-            sumProb += prob;
-        }
+		float sumProb = 0.0f;
+		for (final float prob : probabilities)
+		{
+			sumProb += prob;
+		}
 
-        if (Float.isNaN(sumProb) || Float.isInfinite(sumProb) ||
-                Math.abs(sumProb - 1.0f) < GameConfig.TILE_PROBABILITIES_PRECISION) {
-            throw new RuntimeException("Invalid probabilities of tile creation: sum of probability is not one!");
-        }
+		if (Float.isNaN(sumProb) || Float.isInfinite(sumProb) ||
+				Math.abs(sumProb - 1.0f) < GameConfig.TILE_PROBABILITIES_PRECISION)
+		{
+			throw new RuntimeException("Invalid probabilities of tile creation: sum of probability is not one!");
+		}
 
-        mProbabilities = probabilities;
-        mRandom = random;
-    }
+		mProbabilities = probabilities;
+		mRandom = random;
+	}
 
-    public GameTile Create(int x, int y) {
-        float randomProb = mRandom.nextFloat();
-        float currentProb = 0.0f;
-        int degree = 1;
-        for (float p : mProbabilities) {
-            if (Float.compare(p, 0.0f) != 0 && randomProb < currentProb + p) {
-                break;
-            }
-            degree += 1;
-        }
+	public GameTile Create(int x, int y)
+	{
+		float randomProb = mRandom.nextFloat();
+		float currentProb = 0.0f;
+		int degree = 1;
+		for (float p : mProbabilities)
+		{
+			if (Float.compare(p, 0.0f) != 0 && randomProb < currentProb + p)
+			{
+				break;
+			}
+			degree += 1;
+		}
 
-        return GameTile.CreateWithDegree(x, y, degree);
-    }
+		return GameTile.CreateWithDegree(x, y, degree);
+	}
 }
