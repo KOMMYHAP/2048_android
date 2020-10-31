cca0f7958582a5e858dc1bd552f85db30e9ec313
KOMMYHAP <pasha.u.polyak@gmail.com>
2020-10-31 19:16:23 +0700

    clean up GameActivity

--- client/Android/app/src/main/java/com/wusiko/game2048/ui/game/GameActivity.java
+++ client/Android/app/src/main/java/com/wusiko/game2048/ui/game/GameActivity.java
@@ -2,23 +2,12 @@ package com.wusiko.game2048.ui.game;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.wusiko.game2048.R;
-import com.wusiko.game2048.data.game.CreatedTileLink;
-import com.wusiko.game2048.data.game.MergedTileLink;
-import com.wusiko.game2048.data.game.MovedTileLink;
-import com.wusiko.game2048.ui.utils.OnSwipeTouchListener;
-
-import java.util.List;
-import java.lang.Math;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -26,8 +15,6 @@ import java.lang.Math;
  */
 public class GameActivity extends AppCompatActivity
 {
-	private final String TAG = "GameActivity";
-	private GameBoardViewModel mGameBoardViewModel;
 	private GameBoardView mGameBoardView;
 
 	@Override
@@ -35,103 +22,8 @@ public class GameActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
-		mGameBoardViewModel = new ViewModelProvider(this, new GameBoardViewModelFactory()).get(GameBoardViewModel.class);
 
-		getWindow().getDecorView().setOnTouchListener(new OnSwipeTouchListener(this)
-		{
-			@Override
-			public void onSwipeBottom()
-			{
-				mGameBoardViewModel.OnMoveDown();
-			}
-
-			@Override
-			public void onSwipeRight()
-			{
-				mGameBoardViewModel.OnMoveRight();
-			}
-
-			@Override
-			public void onSwipeLeft()
-			{
-				mGameBoardViewModel.OnMoveLeft();
-			}
-
-			@Override
-			public void onSwipeTop()
-			{
-				mGameBoardViewModel.OnMoveUp();
-			}
-		});
-
-		mGameBoardViewModel.GetCreatedTiles().observe(this, new Observer<List<CreatedTileLink>>()
-		{
-			@Override
-			public void onChanged(List<CreatedTileLink> createdTileLinks)
-			{
-				for (CreatedTileLink link : createdTileLinks)
-				{
-					mGameBoardView.CreateTile(link);
-				}
-			}
-		});
-
-		mGameBoardViewModel.GetMergedTiles().observe(this, new Observer<List<MergedTileLink>>()
-		{
-			@Override
-			public void onChanged(List<MergedTileLink> mergedTileLinks)
-			{
-				for (MergedTileLink link : mergedTileLinks)
-				{
-					int firstTileValue = link.GetFirstTile().getValue();
-					int secondTileValue = link.GetSecondTile().getValue();
-					int resultTileValue = link.GetFirstTile().getValue();
-
-					int[] firstTilePos = link.GetFirstTile().getPosition();
-					int[] secondTilePos = link.GetSecondTile().getPosition();
-					int[] resultPos = link.GetResultTile().getPosition();
-					Log.i(TAG, String.format("Tiles merged: %d (%d; %d) -> %d (%d; %d) = %d (%d; %d)",
-							firstTileValue, firstTilePos[0], firstTilePos[1],
-							secondTileValue, secondTilePos[0], secondTilePos[1],
-							resultTileValue, resultPos[0], resultPos[1]));
-
-					// todo: animate it
-				}
-			}
-		});
-
-		mGameBoardViewModel.GetMovedTiles().observe(this, new Observer<List<MovedTileLink>>()
-		{
-			@Override
-			public void onChanged(List<MovedTileLink> movedTileLinks)
-			{
-				for (MovedTileLink link : movedTileLinks)
-				{
-					int[] from = link.GetPositionFrom();
-					int[] to = link.GetTile().getPosition();
-					int value = link.GetTile().getValue();
-					Log.i(TAG, String.format("Tile moved: %d (%d; %d) -> %d (%d; %d)",
-							value, from[0], from[1],
-							value, to[0], to[1]));
-
-					// todo: animate it
-				}
-			}
-		});
-
-		FrameLayout boardLayout = findViewById(R.id.boardLayout);
-		mGameBoardView = new GameBoardView(this, boardLayout);
-
-		// Apply resizing only when view will be created:
-		FrameLayout boardSizeHelperLayout = findViewById(R.id.boardSizeHelperLayout);
-		boardSizeHelperLayout.post(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				ResizeBoardView();
-			}
-		});
+		mGameBoardView = new GameBoardView(this);
 
 		// Note that some of these constants are new as of API 16 (Jelly Bean)
 		// and API 19 (KitKat). It is safe to use them, as they are inlined
@@ -142,30 +34,16 @@ public class GameActivity extends AppCompatActivity
 				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
-		mGameBoardViewModel.OnRestart();
 	}
 
 	@Override
 	public void onConfigurationChanged(@NonNull Configuration newConfig)
 	{
 		super.onConfigurationChanged(newConfig);
-
 		if (mGameBoardView != null)
 		{
-			ResizeBoardView();
+			mGameBoardView.Resize();
 		}
-	}
-
-	private void ResizeBoardView()
-	{
-		FrameLayout boardSizeHelperLayout = findViewById(R.id.boardSizeHelperLayout);
-		int w = boardSizeHelperLayout.getWidth();
-		int h = boardSizeHelperLayout.getHeight();
-		int size = Math.min(w, h);
-		FrameLayout boardLayout = findViewById(R.id.boardLayout);
-		boardLayout.setLayoutParams(new FrameLayout.LayoutParams(size, size));
-		mGameBoardView.OnResized();
 	}
 
 	@Override
--- client/Android/app/src/main/java/com/wusiko/game2048/ui/game/GameBoardView.java
+++ client/Android/app/src/main/java/com/wusiko/game2048/ui/game/GameBoardView.java
@@ -6,35 +6,145 @@ import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.wusiko.game2048.R;
 import com.wusiko.game2048.data.game.CreatedTileLink;
 import com.wusiko.game2048.data.game.GameConfig;
+import com.wusiko.game2048.data.game.MergedTileLink;
+import com.wusiko.game2048.data.game.MovedTileLink;
+import com.wusiko.game2048.ui.utils.OnSwipeTouchListener;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
+import java.util.List;
 
 @SuppressLint("ViewConstructor")
 public class GameBoardView extends View
 {
 	private final String TAG = "GameBoardView";
 	private final HashMap<Integer, GameTileView> mTileViewsByIndex = new HashMap<>(GameConfig.TILES_NUMBER);
-	private final FrameLayout mLayout;
+	private final FrameLayout mBoardLayout;
+	private final FrameLayout mSizeHelperLayout;
 	private final Context mContext;
+	private final GameBoardViewModel mGameBoardViewModel;
 	private int mTileSize;
 	private int mTileMargin;
 
-	public GameBoardView(Context context, FrameLayout layout)
+	public GameBoardView(AppCompatActivity activity)
 	{
-		super(context);
-		mContext = context;
-		mLayout = layout;
+		super(activity);
+		mContext = activity;
+		mBoardLayout = activity.findViewById(R.id.boardLayout);
+		mSizeHelperLayout = activity.findViewById(R.id.boardSizeHelperLayout);
+		mGameBoardViewModel = new ViewModelProvider(activity, new GameBoardViewModelFactory()).get(GameBoardViewModel.class);
+
+		mSizeHelperLayout.post(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				Resize();
+			}
+		});
+
+		activity.getWindow().getDecorView().setOnTouchListener(new OnSwipeTouchListener(mContext)
+		{
+			@Override
+			public void onSwipeBottom()
+			{
+				mGameBoardViewModel.OnMoveDown();
+			}
+
+			@Override
+			public void onSwipeRight()
+			{
+				mGameBoardViewModel.OnMoveRight();
+			}
+
+			@Override
+			public void onSwipeLeft()
+			{
+				mGameBoardViewModel.OnMoveLeft();
+			}
+
+			@Override
+			public void onSwipeTop()
+			{
+				mGameBoardViewModel.OnMoveUp();
+			}
+		});
+
+		mGameBoardViewModel.GetCreatedTiles().observe(activity, new Observer<List<CreatedTileLink>>()
+		{
+			@Override
+			public void onChanged(List<CreatedTileLink> createdTileLinks)
+			{
+				for (CreatedTileLink link : createdTileLinks)
+				{
+					CreateTile(link);
+				}
+			}
+		});
+
+		mGameBoardViewModel.GetMergedTiles().observe(activity, new Observer<List<MergedTileLink>>()
+		{
+			@Override
+			public void onChanged(List<MergedTileLink> mergedTileLinks)
+			{
+				for (MergedTileLink link : mergedTileLinks)
+				{
+					int firstTileValue = link.GetFirstTile().getValue();
+					int secondTileValue = link.GetSecondTile().getValue();
+					int resultTileValue = link.GetFirstTile().getValue();
+
+					int[] firstTilePos = link.GetFirstTile().getPosition();
+					int[] secondTilePos = link.GetSecondTile().getPosition();
+					int[] resultPos = link.GetResultTile().getPosition();
+					Log.i(TAG, String.format("Tiles merged: %d (%d; %d) -> %d (%d; %d) = %d (%d; %d)",
+							firstTileValue, firstTilePos[0], firstTilePos[1],
+							secondTileValue, secondTilePos[0], secondTilePos[1],
+							resultTileValue, resultPos[0], resultPos[1]));
+
+					// todo: animate it
+				}
+			}
+		});
+
+		mGameBoardViewModel.GetMovedTiles().observe(activity, new Observer<List<MovedTileLink>>()
+		{
+			@Override
+			public void onChanged(List<MovedTileLink> movedTileLinks)
+			{
+				for (MovedTileLink link : movedTileLinks)
+				{
+					int[] from = link.GetPositionFrom();
+					int[] to = link.GetTile().getPosition();
+					int value = link.GetTile().getValue();
+					Log.i(TAG, String.format("Tile moved: %d (%d; %d) -> %d (%d; %d)",
+							value, from[0], from[1],
+							value, to[0], to[1]));
+
+					// todo: animate it
+				}
+			}
+		});
+
+		mGameBoardViewModel.OnRestart();
 	}
 
-	public void OnResized()
+	public void Resize()
 	{
-		final int boardSize = mLayout.getLayoutParams().width;
+		int w = mSizeHelperLayout.getWidth();
+		int h = mSizeHelperLayout.getHeight();
+		int size = Math.min(w, h);
+		mBoardLayout.setLayoutParams(new FrameLayout.LayoutParams(size, size));
+
+		final int boardSize = mBoardLayout.getLayoutParams().width;
 
 		// tile_margin + tile_n_in_a_row * (tile_margin + tile_size) = board_size
 		// tile_margin = tile_size / 8
@@ -48,13 +158,14 @@ public class GameBoardView extends View
 		final int tileSize = 8 * boardSize / (1 + 9 * GameConfig.TILES_IN_A_ROW);
 
 		final float density = getResources().getDisplayMetrics().density;
-		mTileSize = Math.max((int)(density * 64), tileSize);
-		mTileMargin = Math.max((int)(density * 8), mTileSize / 8);
+		mTileSize = Math.max((int) (density * 64), tileSize);
+		mTileMargin = Math.max((int) (density * 8), mTileSize / 8);
 
-		Log.d(TAG, String.format("OnResize: \n" +
-				"    board size = %d\n" +
-				"    tile size = %d\n" +
-				"    tile margin = %d\n",
+		Log.d(TAG, String.format(
+				"OnResize: \n" +
+						"    board size = %d\n" +
+						"    tile size = %d\n" +
+						"    tile margin = %d\n",
 				boardSize,
 				mTileSize,
 				mTileMargin));
@@ -79,13 +190,13 @@ public class GameBoardView extends View
 		tileView.setTranslationY(screenPos[1]);
 
 		mTileViewsByIndex.put(index, tileView);
-		mLayout.addView(tileView);
+		mBoardLayout.addView(tileView);
 
 		Log.d(TAG, String.format(
 				"Tile created: \n" +
-				"    value = %d, \n" +
-				"    board pos = (%d; %d), \n" +
-				"    screen pos = (%d; %d).",
+						"    value = %d, \n" +
+						"    board pos = (%d; %d), \n" +
+						"    screen pos = (%d; %d).",
 				link.GetTile().getValue(),
 				boardPos[0], boardPos[1],
 				screenPos[0], screenPos[1]));
