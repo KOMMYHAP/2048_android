abe20875f0e51721934e4877e79e40b56d8f6e14
KOMMYHAP <pasha.u.polyak@gmail.com>
2020-11-30 22:35:29 +0700

    add resources for 2, 4, 8, ..., 2048 tiles

--- client/Android/app/src/main/java/com/wusiko/game2048/ui/game/GameTileView.java
+++ client/Android/app/src/main/java/com/wusiko/game2048/ui/game/GameTileView.java
@@ -29,7 +29,34 @@ public class GameTileView extends androidx.appcompat.widget.AppCompatImageView
 
 	private static int DegreeToResource(int degree)
 	{
-		// stub
-		return R.drawable.test;
+		final int value = 1 << degree;
+		switch (value)
+		{
+			case 2:
+				return R.drawable.t2;
+			case 4:
+				return R.drawable.t4;
+			case 8:
+				return R.drawable.t8;
+			case 16:
+				return R.drawable.t16;
+			case 32:
+				return R.drawable.t32;
+			case 64:
+				return R.drawable.t64;
+			case 128:
+				return R.drawable.t128;
+			case 256:
+				return R.drawable.t256;
+			case 512:
+				return R.drawable.t512;
+			case 1024:
+				return R.drawable.t1024;
+			case 2048:
+				return R.drawable.t2048;
+			// todo: add all possible value
+		}
+		// todo: add image to indicate invalid value
+		return R.drawable.t2;
 	}
 }
